32bc84bf54d966d04b7e35e5bb96476f39ebb35e
RafaelCompri <54962086+RafaCompri@users.noreply.github.com>
2022-04-29 22:00:31 -0300

    Add files via upload

--- Exec3.py
+++ Exec3.py
@@ -2,45 +2,58 @@ import socket
 
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((socket.gethostname(), 6969))
-    print(server)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+
+
+    servidor.bind((socket.gethostname(), 8888))
+    
     server.listen()
+
+
     while True:
-        conexao, addr = server.accept()
+        adicionado, conectado = server.accept()
         with conexao:
-            print(f"Client connected: {addr}")
+            
+	print(f"Conectado: {adicionado}")
             while True:
-                sent_data = conexao.recv(1024)
                 
-                if not sent_data:
-                    break 
+		dados = conectado.recv(1024)
+                
+                
                 decoded_data = sent_data.decode()
                 
-                data_list = decoded_data.split("|")
-               
-                inicial_code = int(data_list[0])
-                n = int(data_list[1])
                 
-                if(inicial_code < 10000000):
-                    conexao.sendall(b"The inicial code is not within range. It must be higher than 12.000.000")
+               if not dados:
+                    break 
+
+                inicializacao = int(lista[0])
+               	q = int(data_list[1])
+                
+                if(inicializacao < 10000000):
+                    
                     break
                
-                if(n < 5000 or n > 15000):
-                    conexao.sendall(b"The n is not within range. It must be between 5000 and 15000.")
+                if(q < 5000 or q > 15000):
+                    
                     break
                 
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as process_client:
+
                     process_client.connect(("192.168.0.5", 6969))
-                    print(process_client)
+                    
                    
-                    message_to_send = str(inicial_code) + "|" + str(n)
+                    message_to_send = str(inicializacao) + str(q)
                     
                     process_client.sendall(message_to_send.encode())
                     
                     data = process_client.recv(1024)
-                    print(f"Answer from server: {data.decode()}")
+                    print(f"{data.decode()}")
                 
-                conexao.sendall(data)
-                print("Client disconnected.")
+                
+
+
+		conexao.sendall(data)
+
+
+                print("Desconectado.")
                 print()
